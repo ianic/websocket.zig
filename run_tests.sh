a93ffc0d60dafc52fb4d69780a26a199b7b88bd2
Igor Anić <igor.anic@gmail.com>
2022-12-21 14:40:03 +0100

    add test runner

---
+++ run_tests.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -ex
+
+zig build -freference-trace -Drelease-fast
+#zig build -freference-trace
+
+url=ws://localhost:9001
+msgs=$(websocat "$url/getCaseCount" -E --jsonrpc)
+
+./zig-out/bin/autobahn_client2 "$msgs"
+websocat "$url/updateReports?agent=websocket.zig" -E
