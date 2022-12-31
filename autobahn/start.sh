d2a3f1de62f4bedff16009440fd4810ca45752f2
Igor Anić <igor.anic@gmail.com>
2022-12-31 19:31:38 +0100

    add test results

---
+++ autobahn/start.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash -e
+
+docker run -it --rm \
+    -v "${PWD}/config:/config" \
+    -v "${PWD}/reports:/reports" \
+    --name fuzzingserver \
+    -p 9001:9001 \
+    -p 8080:8080 \
+    crossbario/autobahn-testsuite:0.8.2 \
+    wstest --mode fuzzingserver --spec /config/fuzzingserver.json -d
