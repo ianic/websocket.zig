0121f829f88a4f88000c473bf5731563c889cf75
Igor Anić <igor.anic@gmail.com>
2023-01-03 22:34:38 +0100

    add few example configurations

--- autobahn/start.sh
+++ autobahn/start.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash -e
 
+docker stop fuzzingserver || true
+
 docker run -it --rm \
     -v "${PWD}/config:/config" \
     -v "${PWD}/reports:/reports" \
@@ -7,4 +9,4 @@ docker run -it --rm \
     -p 9001:9001 \
     -p 8080:8080 \
     crossbario/autobahn-testsuite:0.8.2 \
-    wstest --mode fuzzingserver --spec /config/fuzzingserver.json -d
+    wstest --mode fuzzingserver --spec /config/all.json
