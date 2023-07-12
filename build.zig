d23673385a0276b1b0a8bc311dfb7650d6d49d28
Igor Anić <igor.anic@gmail.com>
2023-07-12 21:44:35 +0200

    make build pass on zig master

--- build.zig
+++ build.zig
@@ -1,31 +1,52 @@
 const std = @import("std");
-const ws = @import("websocket.zig");
+const zlib_build = @import("zlib/zlib.zig");
 
-pub fn build(b: *std.build.Builder) void {
-    // Standard release options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
-    const mode = b.standardReleaseOptions();
+pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
 
-    const lib = ws.create(b, target, mode);
-    lib.step.install();
+    const zlib = zlib_build.create(b, target, optimize);
+    //b.installArtifact(zlib.step);
 
-    const main_tests = b.addTest("src/main.zig");
-    lib.link(main_tests);
-    main_tests.setBuildMode(mode);
+    const zlib_module = b.addModule("zlib", .{
+        .source_file = .{ .path = "zlib/src/main.zig" },
+    });
+    const ws_module = b.addModule("ws", .{
+        .source_file = .{ .path = "src/main.zig" },
+        .dependencies = &[_]std.Build.ModuleDependency{.{ .name = "zlib", .module = zlib_module }},
+    });
 
-    const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&main_tests.step);
+    const ws_lib = b.addStaticLibrary(.{
+        .name = "ws",
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+    zlib.link(ws_lib, .{ .import_name = "zlib" });
+    b.installArtifact(ws_lib);
 
-    const example_step = b.step("examples", "Build examples");
-    inline for (.{
-        "autobahn_client",
-    }) |example_name| {
-        const exe = b.addExecutable(example_name, "examples/" ++ example_name ++ ".zig");
-        lib.link(exe);
-        exe.setBuildMode(mode);
-        exe.setTarget(target);
-        exe.install();
-        example_step.dependOn(&exe.step);
-    }
+    // zig build test
+    const test_compile = b.addTest(.{
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+    zlib.link(test_compile, .{ .import_name = "zlib" });
+
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&test_compile.step);
+
+    // build examples
+    const bin = b.addExecutable(.{
+        .name = "autobahn_client",
+        .root_source_file = .{ .path = "examples/autobahn_client.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+    bin.linkLibrary(ws_lib);
+    bin.addModule("ws", ws_module);
+    b.installArtifact(bin);
 }
+
+// to test single file
+// $ zig test src/main.zig --deps zlib=zlib --mod zlib::zlib/src/main.zig -l z
