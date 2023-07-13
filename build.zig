004cfd77040d9a582077bafe3ea0209e3583520f
Igor Anić <igor.anic@gmail.com>
2023-07-13 18:58:15 +0200

    use zlib from package manager

--- build.zig
+++ build.zig
@@ -1,42 +1,48 @@
 const std = @import("std");
-const zlib_build = @import("zlib/zlib.zig");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const zlib = zlib_build.create(b, target, optimize);
-    //b.installArtifact(zlib.step);
+    // Define dependencies.
+    const zlib = b.dependency("zlib", .{});
 
-    const zlib_module = b.addModule("zlib", .{
-        .source_file = .{ .path = "zlib/src/main.zig" },
-    });
+    // Define module
     const ws_module = b.addModule("ws", .{
         .source_file = .{ .path = "src/main.zig" },
-        .dependencies = &[_]std.Build.ModuleDependency{.{ .name = "zlib", .module = zlib_module }},
+        .dependencies = &[_]std.Build.ModuleDependency{.{ .name = "zlib", .module = zlib.module("zlib") }},
     });
 
+    // Build library.
     const ws_lib = b.addStaticLibrary(.{
         .name = "ws",
         .root_source_file = .{ .path = "src/main.zig" },
         .target = target,
         .optimize = optimize,
     });
-    zlib.link(ws_lib, .{ .import_name = "zlib" });
+    // Link z library and zlib module.
+    ws_lib.linkLibrary(b.dependency("zlib", .{
+        .target = target,
+        .optimize = optimize,
+    }).artifact("z"));
+    ws_lib.addModule("zlib", zlib.module("zlib"));
     b.installArtifact(ws_lib);
 
-    // zig build test
+    // Build test.
     const test_compile = b.addTest(.{
         .root_source_file = .{ .path = "src/main.zig" },
         .target = target,
         .optimize = optimize,
     });
-    zlib.link(test_compile, .{ .import_name = "zlib" });
-
+    test_compile.linkLibrary(b.dependency("zlib", .{
+        .target = target,
+        .optimize = optimize,
+    }).artifact("z"));
+    test_compile.addModule("zlib", zlib.module("zlib"));
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&test_compile.step);
 
-    // build examples
+    // Build examples.
     const bin = b.addExecutable(.{
         .name = "autobahn_client",
         .root_source_file = .{ .path = "examples/autobahn_client.zig" },
