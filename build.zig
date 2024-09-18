066943b29e0807115b547f4e086e59545b8edc4a
nk521 <nk_mason@protonmail.com>
2024-09-19 01:16:31 +0530

    chore: upgrade to 0.13

--- build.zig
+++ build.zig
@@ -5,18 +5,14 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     // Define dependencies.
-    const zlib = b.dependency("zlib", .{});
-
+    const zlib_module = b.dependency("zlib", .{ .target = target, .optimize = optimize }).module("zlib");
     // Define module
-    const ws_module = b.addModule("ws", .{
-        .source_file = .{ .path = "src/main.zig" },
-        .dependencies = &[_]std.Build.ModuleDependency{.{ .name = "zlib", .module = zlib.module("zlib") }},
-    });
+    // const ws_module = b.addModule("ws", .{ .root_source_file = b.path("src/main.zig"), .imports = &.{.{ .name = "zlib", .module = zlib_module }} });
 
     // Build library.
     const ws_lib = b.addStaticLibrary(.{
         .name = "ws",
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
@@ -25,12 +21,12 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     }).artifact("z"));
-    ws_lib.addModule("zlib", zlib.module("zlib"));
+    ws_lib.root_module.addImport("zlib", zlib_module);
     b.installArtifact(ws_lib);
 
     // Build test.
     const test_compile = b.addTest(.{
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
@@ -38,30 +34,31 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     }).artifact("z"));
-    test_compile.addModule("zlib", zlib.module("zlib"));
+    test_compile.root_module.addImport("zlib", zlib_module);
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&test_compile.step);
 
     // Build examples.
-    var bin = b.addExecutable(.{
-        .name = "autobahn_client",
-        .root_source_file = .{ .path = "examples/autobahn_client.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
-    bin.linkLibrary(ws_lib);
-    bin.addModule("ws", ws_module);
-    b.installArtifact(bin);
+    // var bin = b.addExecutable(.{
+    //     .name = "autobahn_client",
+    //     .root_source_file = b.path("examples/autobahn_client.zig"),
+    //     .target = target,
+    //     .optimize = optimize,
+    // });
+    // bin.linkLibrary(ws_lib);
+    // bin.root_module.addImport("ws", ws_module);
+    // bin.root_module.addImport("zlib", zlib_module);
+    // b.installArtifact(bin);
 
-    bin = b.addExecutable(.{
-        .name = "wss",
-        .root_source_file = .{ .path = "examples/wss.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
-    bin.linkLibrary(ws_lib);
-    bin.addModule("ws", ws_module);
-    b.installArtifact(bin);
+    // bin = b.addExecutable(.{
+    //     .name = "wss",
+    //     .root_source_file = b.path("examples/wss.zig"),
+    //     .target = target,
+    //     .optimize = optimize,
+    // });
+    // bin.linkLibrary(ws_lib);
+    // bin.root_module.addImport("ws", ws_module);
+    // b.installArtifact(bin);
 }
 
 // to test single file
