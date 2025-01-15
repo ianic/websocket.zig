c32dcab4e59bafd87e40a279dc482a139c103244
Igor Anić <igor.anic@gmail.com>
2025-01-15 13:03:06 +0100

    rethinking async api

--- src/main.zig
+++ src/main.zig
@@ -24,7 +24,7 @@ test {
 const Options = @import("stream.zig").Options;
 
 pub const asyn = struct {
-    pub fn Client(comptime ChildType: type) type {
+    pub fn Client(comptime Upstream: type, comptime Downstream: type) type {
         const HandshakeType = handshake.Client(
             std.io.FixedBufferStream([]u8).Reader,
             std.ArrayList(u8).Writer,
@@ -34,41 +34,58 @@ pub const asyn = struct {
             const Self = @This();
 
             allocator: std.mem.Allocator,
-            child: ChildType,
+            upstream: Upstream,
+            downstream: Downstream,
             uri: []const u8,
             opt: Options = .{},
             handshake: ?*HandshakeType = null,
-            conn: Conn(ChildType) = undefined,
+            conn: Conn(Upstream, Downstream),
 
-            pub fn init(allocator: std.mem.Allocator, child: ChildType, uri: []const u8) Self {
+            pub fn init(
+                allocator: std.mem.Allocator,
+                upstream: Upstream,
+                downstream: Downstream,
+                uri: []const u8,
+            ) Self {
                 return .{
                     .allocator = allocator,
-                    .child = child,
+                    .upstream = upstream,
+                    .downstream = downstream,
                     .uri = uri,
-                    .conn = Conn(ChildType).init(allocator, child),
+                    .conn = Conn(Upstream, Downstream).init(allocator, upstream, downstream),
                 };
             }
 
             pub fn deinit(self: *Self) void {
-                if (self.handshake) |hs|
+                if (self.handshake) |hs| {
+                    hs.deinit();
                     self.allocator.destroy(hs);
+                    self.handshake = null;
+                }
+                self.conn.deinit();
             }
 
-            pub fn onConnect(self: *Self) !void {
-                const hs = try self.allocator.create(HandshakeType);
+            pub fn connect(self: *Self) !void {
                 var list = std.ArrayList(u8).init(self.allocator);
                 defer list.deinit();
+
+                const hs = try self.allocator.create(HandshakeType);
+                errdefer {
+                    self.allocator.destroy(hs);
+                    self.handshake = null;
+                }
                 hs.* = HandshakeType.init(self.allocator, undefined, list.writer());
                 self.handshake = hs;
+
                 try hs.writeRequest(self.uri);
-                try self.child.wsSendZc(try list.toOwnedSlice());
+                try self.downstream.sendZc(try list.toOwnedSlice());
             }
 
             pub fn onSend(self: *Self, buf: []const u8) void {
                 self.allocator.free(buf);
             }
 
-            pub fn onRecv(self: *Self, bytes: []u8) !usize {
+            pub fn recv(self: *Self, bytes: []u8) !usize {
                 if (self.handshake) |hs| {
                     var fbs = std.io.fixedBufferStream(bytes);
                     hs.reader = fbs.reader();
@@ -82,10 +99,10 @@ pub const asyn = struct {
                     self.allocator.destroy(hs);
                     self.handshake = null;
 
-                    try self.child.onWsHandshake();
-                    return n + try self.conn.onRecv(bytes[n..]);
+                    self.upstream.onConnect();
+                    return n + try self.conn.recv(bytes[n..]);
                 }
-                return try self.conn.onRecv(bytes);
+                return try self.conn.recv(bytes);
             }
 
             pub fn send(self: *Self, bytes: []const u8) !void {
@@ -98,25 +115,34 @@ pub const asyn = struct {
     const mem = std.mem;
     const assert = std.debug.assert;
 
-    pub fn Conn(comptime ChildType: type) type {
+    pub fn Conn(comptime Upstream: type, comptime Downstream: type) type {
         return struct {
             const Self = @This();
 
             allocator: mem.Allocator,
-            child: ChildType,
+            upstream: Upstream,
+            downstream: Downstream,
 
             last_frame_fragment: Frame.Fragment = .unfragmented,
             message: ?Message = null,
 
-            pub fn init(allocator: mem.Allocator, child: ChildType) Self {
-                return .{ .allocator = allocator, .child = child };
+            pub fn init(
+                allocator: mem.Allocator,
+                upstream: Upstream,
+                downstream: Downstream,
+            ) Self {
+                return .{
+                    .allocator = allocator,
+                    .upstream = upstream,
+                    .downstream = downstream,
+                };
             }
 
             pub fn deinit(self: *Self) void {
                 if (self.message) |msg| msg.deinit();
             }
 
-            pub fn onRecv(self: *Self, bytes: []u8) !usize {
+            pub fn recv(self: *Self, bytes: []u8) !usize {
                 var n: usize = 0;
                 while (n < bytes.len) {
                     const frm, const frm_len = Frame.parse(bytes[n..]) catch |err| switch (err) {
@@ -128,7 +154,7 @@ pub const asyn = struct {
                         // TODO
                         // try self.child.onControlFrame(frm);
                     } else {
-                        try self.onFrame(frm);
+                        try self.recvFrame(frm);
                     }
                     n += frm_len;
                 }
@@ -139,14 +165,14 @@ pub const asyn = struct {
                 const frame = Frame{ .fin = 1, .rsv1 = 0, .opcode = .text, .payload = bytes, .mask = 1 };
                 const buf = try self.allocator.alloc(u8, frame.encodedLen());
                 _ = frame.encode(buf, 0);
-                try self.child.wsSendZc(buf);
+                try self.downstream.sendZc(buf);
             }
 
             pub fn onSend(self: *Self, buf: []const u8) void {
                 self.allocator.free(buf);
             }
 
-            fn onFrame(self: *Self, frm: Frame) !void {
+            fn recvFrame(self: *Self, frm: Frame) !void {
                 try frm.assertValidContinuation(self.last_frame_fragment);
                 self.last_frame_fragment = frm.fragment();
                 switch (frm.fragment()) {
@@ -158,7 +184,7 @@ pub const asyn = struct {
                             .payload = frm.payload,
                         };
                         try msg.validate();
-                        try self.child.onWsMessage(msg);
+                        self.upstream.onMessage(msg);
                     },
                     .start => {
                         assert(self.message == null);
@@ -181,7 +207,7 @@ pub const asyn = struct {
                             msg.deinit();
                             self.message = null;
                         }
-                        try self.child.onWsMessage(msg.*);
+                        self.upstream.onMessage(msg.*);
                     },
                 }
             }
